598060da709e06dfe916bae070b15b73f8fc202e
abdoelrochman24-dev <abdoelrochman24@gmail.com>
2025-10-23 15:36:24 +0700

    Delete soal2.py

--- soal2.py
+++
@@ -1,9 +0,0 @@
-kendaraan = ["namaKendaraan", "jenisKendaraan", "ccKendaraan", "warnaKendaraan", "rodaKendaraan"]
-
-kendaraan.extend(["hargaNomor", "tipeKendaraan"])
-
-kendaraan.insert(2, "merkKendaraan")
-
-print(kendaraan)
-
-['namaKendaraan', 'jenisKendaraan', 'merkKendaraan', 'ccKendaraan', 'warnaKendaraan', 'rodaKendaraan', 'hargaNomor', 'tipeKendaraan']
